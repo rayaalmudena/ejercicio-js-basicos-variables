354151b45dab70321793fd5bc4011bbeebc67a21
rayaalmudena <36917373+rayaalmudena@users.noreply.github.com>
2022-02-28 13:54:25 +0100

    Update ex6.js

--- ex6.js
+++ ex6.js
@@ -14,6 +14,9 @@
 
 let numeroPi = Math.PI
 console.log("Valor de PI:", numeroPi)
+let radio = 8;
+let valor = numeroPi*Math.pow(radio,2);
+console.log("El area de un círculo de radio "+radio+" es de "+valor.toFixed(2)+" m2");
 
 /**
  * BONUS: Muestra el mensaje de tal manera que el número devuelto esté redondeado a dos decimales. Es decir, para un radio de 3, el mensaje debería ser: ""El area de un círculo de radio 3 es de 28.27 m2"
